cdf4ddbe2ef0b9a80f25826eb67c6eccf45a906a
kimbareum <right.kim93@gmail.com>
2023-08-20 22:08:24 +0900

    Refactor: DRF View 추가

--- chat/urls.py
+++ chat/urls.py
@@ -7,4 +7,5 @@ app_name = 'chat'
 urlpatterns = [
     path("", views.index, name="index"),
     path("<int:room_id>/", views.chatRoomView.as_view(), name="room"),
+    path("API/<int:room_id>/", views.ChatRoomAPIView.as_view(), name="roomAPI"),
 ]
--- chat/views.py
+++ chat/views.py
@@ -5,6 +5,7 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.views import View
 
 from rest_framework.decorators import APIView
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
@@ -18,7 +19,7 @@ User = get_user_model()
 
 
 def room_join_permission(chat_room, user):
-    chat_room_join = ChatRoomJoin.objects.filter(user = user, chatroom=chat_room)
+    chat_room_join = ChatRoomJoin.objects.filter(user=user, chatroom=chat_room)
     if chat_room_join.exists():
         return True
     return False
@@ -61,6 +62,38 @@ class chatRoomView(View):
             chat_room = ChatRoom.objects.create(owner=user)
             get_room_join_permission(chat_room, user)
         return self.chat_room_render(request, chat_room, user)
+    
+
+class ChatRoomAPIView(APIView):
+
+    def chat_room_render(self, chat_room, user):
+        messages = ChatMessage.objects.filter(chatroom = chat_room.pk)
+        serialized_messages = ChatMessageSerializer(instance=messages, many=True)
+        serialized_user = {"user_id": user.id, "user_username": user.username}
+        return Response({"room_id": chat_room.pk, "messages":serialized_messages.data, "user": serialized_user}, status=status.HTTP_200_OK)
+    
+
+    def post(self, request, room_id):
+        # user = request.user
+        user = User.objects.get(pk=1)
+        try:
+            chat_room = ChatRoom.objects.get(pk=room_id)
+        except ObjectDoesNotExist:
+            chat_room = ChatRoom.objects.create(owner=user)
+        get_room_join_permission(chat_room, user)
+        return self.chat_room_render(chat_room, user)
+        
+    
+    def get(self, request, room_id):
+        # user = request.user
+        user = User.objects.get(pk=1)
+        try:
+            chat_room = ChatRoom.objects.get(pk=room_id)
+            if not room_join_permission(chat_room, user):
+                return Response("채팅방에 접근할 권한이 없습니다.", status=status.HTTP_401_UNAUTHORIZED)
+        except ObjectDoesNotExist:
+            return Response("채팅방이 존재하지 않습니다.", status=status.HTTP_400_BAD_REQUEST)
+        return self.chat_room_render(chat_room, user)
 
 
 # # @APIView
