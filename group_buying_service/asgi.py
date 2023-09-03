bb5d3635c1b18e2d1b3a2bff6ba8f71430fe798f
kimbareum <right.kim93@gmail.com>
2023-09-03 17:22:42 +0900

    Fix: daphne 관련 설정 재 수정

--- group_buying_service/asgi.py
+++ group_buying_service/asgi.py
@@ -8,23 +8,19 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
-
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
-from django.contrib.auth import get_user_model
-
-from user.backends import JWTAuthMiddleware
-
-from chat.routing import websocket_urlpatterns
+from channels.routing import ProtocolTypeRouter, URLRouter
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "group_buying_service.settings")
-
 django_asgi_app = get_asgi_application()
 
-User = get_user_model()
+from channels.auth import AuthMiddlewareStack
+from channels.security.websocket import AllowedHostsOriginValidator
+from django.contrib.auth import get_user_model
+from user.backends import JWTAuthMiddleware
+from chat.routing import websocket_urlpatterns
 
+User = get_user_model()
 
 application = ProtocolTypeRouter(
     {
