8dc268e397e5784d44d183acde51862bc4915ecf
kimbareum <right.kim93@gmail.com>
2023-09-03 23:40:51 +0900

    누락된 로컬 수정사항 반영

--- group_buying_service/settings.py
+++ group_buying_service/settings.py
@@ -103,21 +103,11 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("127.0.0.1", 6379)],
+            "hosts": [(config('REDIS_HOST'), config('REDIS_PORT'))],
         },
     },
 }
 
-
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
 DATABASES = {
     'default': {
